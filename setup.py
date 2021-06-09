0150a34f840672c04e88218f5da95612ff385f4e
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-06-09 10:53:20 -0700

    Update requirements parsing (#2)

--- setup.py
+++ setup.py
@@ -20,8 +20,28 @@
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from setuptools import setup, find_packages
+from os import path, getenv
 
 PLUGIN_ENTRY_POINT = 'mozilla_remote = neon_tts_plugin_mozilla_remote:MozillaRemoteTTS'
+
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
+
+    for i in range(0, len(requirements)):
+        r = requirements[i]
+        if "@" in r:
+            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
+            r = "@".join(parts)
+            if getenv("GITHUB_TOKEN"):
+                if "github.com" in r:
+                    r = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
+            requirements[i] = r
+    return requirements
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -34,9 +54,6 @@ with open("./version.py", "r", encoding="utf-8") as v:
             else:
                 version = line.split("'")[1]
 
-with open("./requirements.txt", "r", encoding="utf-8") as r:
-    requirements = r.readlines()
-
 setup(
     name='neon-tts-plugin-mozilla_remote',
     version=version,
@@ -46,7 +63,7 @@ setup(
     author_email='developers@neon.ai',
     license='NeonAI License v1.0',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=get_requirements("requirements.txt"),
     zip_safe=True,
     classifiers=[
         'Intended Audience :: Developers',
